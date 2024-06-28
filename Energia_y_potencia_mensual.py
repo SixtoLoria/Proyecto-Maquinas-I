778fafb0c256f9179201f88a5757d59aa2e02824
Yoel Moya Carmona <yoel.moya.carmona@gmail.com>
2024-06-28 14:04:37 -0600

    Actualizando e imprimiendo algunos valores

--- Energia_y_potencia_mensual.py
+++ Energia_y_potencia_mensual.py
@@ -21,8 +21,8 @@ df = df.dropna(subset=['Potencia'])
 energia_mensual_kWh = (1/7) * (1/12000) * df['Potencia'].sum()      # muestras cada 5 min, convertido a kWh
 potencia_mensual_kW = df['Potencia'].mean()   / 1000                # Promedio de potencia en Kwatts
 
-print(f"Energía mensual en kWh: {energia_mensual_kWh:.2f}")
-print(f"Potencia mensual en KW: {potencia_mensual_kW:.2f}")
+print(f"Energía mensual consumida en kWh: {energia_mensual_kWh:.2f}")
+print(f"Potencia mensual consumida en KW: {potencia_mensual_kW:.2f}")
 
 Costo = 0
 
@@ -40,7 +40,7 @@ else:
     Costo = energia_mensual_kWh * 79.96
     
     
-print(f"El costo es {Costo:.2f}")
+print(f"El costo mensual de la energia consumida es {Costo:.2f}")
 
 
 # ------------------- calculo de perdidas -----------------
@@ -55,7 +55,13 @@ Perdidas_mensuales_kW = ( P_FE + P_cu * (df['Potencia'].mean()/S_nom * fp) ** 2 
 
 
 print(f"Las perdidas mensuales son en kW:  {Perdidas_mensuales_kW:.2f}")
-print(f"Las perdidas mensuales son en kWh: {Perdidas_mensuales_kW:.2f}")
+print(f"Las perdidas mensuales son en kWh: {Perdidas_mensuales_kWh:.2f}")
+
+Perdidas_anuales_kWh = Perdidas_mensuales_kWh *12           
+Perdidas_anuales_kW = Perdidas_mensuales_kW *12
+
+print(f"Las perdidas mensuales son en kW:  {Perdidas_anuales_kW:.2f}")
+print(f"Las perdidas mensuales son en kWh: {Perdidas_anuales_kWh:.2f}")
 
 
 # ------------------- calculo del costo con perdidas -----------------
@@ -71,5 +77,14 @@ elif (kWh_totales) > 3000 and (kW_totales) > 8:
     Costo_P = (kWh_totales) * 46.03 + ((kW_totales) * 7454.90)
 else:
     Costo_P = (kWh_totales) * 79.96
-    
-print(f"El costo es considerando perdidas es: {Costo_P:.2f}")
+
+Costo_P_Anual = Costo_P * 12
+print(f"El costo mensual, considerando perdidas es: {Costo_P:.2f}")
+print(f"El costo anual, considerando perdidas es: {Costo_P_Anual:.2f}")
+print("\nNOTA: Todos los costos son en colones.\n")
+
+
+
+
+
+
